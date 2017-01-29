94c183c0f19ae8cf7fdd79183ccdd7660cbf5c41
calebomusic <calebomusic@gmail.com>
2017-01-28 21:32:32 -0800

    jQuery Lite init with initial DOMNodeCollection methods

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./lib/main.js",
+  output: {
+  	filename: "./lib/jquery_lite.js"
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
